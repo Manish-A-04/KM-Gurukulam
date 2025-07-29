2fc82c86f3835e854ff8fa32d558846cda4f8c79
Manish-A-04 <manishprofessional04@gmail.com>
2025-07-29 21:11:52 +0530

    Changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+   basePath: '/KM-Gurukulam',
+   assetPrefix: '/KM-Gurukulam',
+   trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
